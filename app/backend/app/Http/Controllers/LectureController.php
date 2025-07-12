511d059ca767c49f9324ac48dc9d871f3a662598
iniad <s1f102301336@iniad.org>
2025-07-12 23:20:38 +0900

    講義履歴取得APIを接続完了

--- app/backend/app/Http/Controllers/LectureController.php
+++ app/backend/app/Http/Controllers/LectureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lecture;
 use Illuminate\Http\Request;
 
 class LectureController extends Controller
@@ -12,7 +13,7 @@ class LectureController extends Controller
         // 例えば、Lectureモデルを使ってデータベースから履歴を取得するなど。
 
         // 今回はダミーデータを返す例です。
-        $lectures = Lecture::all(); // ここでLectureモデルを使ってデータベースから履歴を取得することもできます。
+        $lectures = Lecture::all();
 
         return response()->json($lectures);
     }
